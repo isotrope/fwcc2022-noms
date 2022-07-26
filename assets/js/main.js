ecf3627d3549d7ce3e743d0404fd381656b43df2
mbluma@lesaffranchis.ca <xGxs3apYUmVkbh2UDpxD>
2022-07-26 08:42:31 -0400

    Première version moussaillon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,139 +1,136 @@
-const name = 'nom',
-    adjective = 'adjectif';
-
-const modes = [
-  {
-    "limitUp": 0.33,
-    "limitDown": 0,
-    "label": "N+A",
-    "config": [name, adjective]
-  },
-  {
-    "limitUp": 0.66,
-    "limitDown": 0.34,
-    "label": "A+N",
-    "config": [adjective, name]
-  },
-  {
-    "limitUp": 1,
-    "limitDown": 0.67,
-    "label": "N+N",
-    "config": [name, name]
-  }
-];
-
-
-function getRandomIndex(targetArray) {
-  return Math.floor(Math.random() * targetArray.length);
+const data = {
+	"prefixes" : [
+		"O'",
+		"Mac",
+		"Van Der",
+		"Fitz",
+	],
+	"noms"     : [
+		"Moppe ",
+		"Yarrrr",
+		"Calico",
+		"Marsouin",
+		"Barbu",
+		"Canon",
+		"Poulpe",
+		"Teigneux ",
+		"Bigleux ",
+		"Borgne",
+		"Crapule",
+		"Scorbut",
+		"Serpilleur",
+		"Croquette",
+		"Barbiche",
+		"Plongeon",
+		"Espadon",
+		"Poisseux",
+		"Barracuda",
+		"Tuba",
+		"Piranha ",
+		"Morbleu",
+		"Mousse",
+		"Babord",
+		"Boucane",
+		"Grog",
+		"Butin",
+		"Sloop",
+		"Bamboche",
+		"Bourlingue",
+		"Boujaron",
+		"Caboulot",
+		"Cambuse",
+		"Rafiot",
+		"Frégate",
+		"Sapristi",
+		"Parsambleu",
+		"Poisson",
+		"Mouille",
+		"Marigane ",
+		"Crapet",
+		"Méné",
+		"Barbotte",
+		"Truite",
+		"Anchois",
+		"Brochet",
+		"Dorade",
+		"Éperlan",
+		"Flétan",
+		"Gallinette",
+		"Hareng",
+		"Murène",
+		"Briochette",
+		"Nemo",
+	],
+	"dit"      : [
+		"Chuchoteur de perroquets",
+		"Sabre ébréché",
+		"Squelette sec",
+		"Homard houspillé ",
+		"Maraudeur marin",
+		"Pont glissant",
+		"Banc de corail",
+		"Loup de mer",
+		"Barbe blonde",
+		"Pionnier des cales mouillées",
+		"Seau troué",
+		"Naufragé",
+		"Pendu",
+		"Rebelle",
+		"Gredin",
+		"Jolly Roger",
+		"Bois-sans-soif",
+		"Molaston",
+		"Tonnelier",
+		"Bachi-bouzouk",
+		"Fruit de caquet",
+		"Fièvre du Cap Horn",
+		"Marin d'eau douce",
+		"Roule-caillou",
+		"Mulet",
+		"Tête-de-boule",
+		"Raseux-de-terre",
+		"Requin pyjama",
+		"Merlu",
+	],
 }
 
 
-function getIndexValue(targetArray, index= -1)
-{
-  if (index > 0) {
-    return targetArray[index];
-  } else {
-    return targetArray[getRandomIndex(targetArray)];
-  }
+function getRandomName() {
+	const randomPrefix = getRandomElement( data.prefixes );
+	const randomName   = getRandomElement( data.noms );
+	const randomDit    = getRandomElement( data.dit );
+
+
+	return [
+		'<span class="prefixe">' + randomPrefix + '</span>',
+		'<span class="nom">' + randomName + '</span>',
+		'<span class="dit"><span class="dit-delimiter">dit </span> <span class="word-value">«&nbsp;' + randomDit + '&nbsp;»</span></span>',
+	].join("\n");
+
+}
+
+function getRandomElement( itemsCollection ) {
+	return itemsCollection[ Math.floor( Math.random() * itemsCollection.length ) ];
 }
 
 
-function getMode() {
-  let rand = Math.random(),
-      returnMode = modes[0];
-  modes.forEach(function(mode) {
-    if (rand >= mode.limitDown && rand <= mode.limitUp) {
-      returnMode = mode;
-    }
-  });
-  return returnMode;
+const btnGenerate   = document.querySelector( '#regenerate' ),
+	  namePlacement = document.querySelector( '#result' );
+
+btnGenerate.addEventListener( 'click', displayRandomName )
+
+
+function displayRandomName() {
+	const randomName = getRandomName();
+
+	namePlacement.innerHTML = randomName;
 }
 
-
-function addWordIn(word, targetData) {
-  let canAddWord = true;
-  //check if the word already exist.
-  targetData.forEach(function(element) {
-    if (word === element) {
-      canAddWord = false;
-    }
-  });
-
-  if (canAddWord) {
-    //add the word to.
-    targetData.push(word);
-  }
-  return targetData;
-}
+displayRandomName();
 
 
-/**
- * Main action
- * triggered on Click of "encore" btn.
- * And on Start.
- */
-function douxItte() {
-  let noms = data.noms,
-      adjectifs = data.adjectifs,
-      resultat = [],
-      currentMode = getMode();
-
-  currentMode.config.forEach(function(word, index) {
-    let targetWord = "";
-    if (word === name) {
-      targetWord = getIndexValue(noms);
-    }
-    if (word === adjective) {
-      targetWord = getIndexValue(adjectifs);
-    }
-    if (index > 0) {
-      targetWord = targetWord.toLowerCase();
-    }
-    resultat.push(targetWord.toString());
-  });
-
-  resultat = resultat.join(" ");
-
-  addHistoryLine("["+currentMode.label+"] "+ resultat);//render automatically.
-  renderResult("result", resultat);
-}
-
-
-function addNom() {
-  let newNom = document.getElementById("new_nom").value;
-  data.noms = addWordIn(newNom, data.noms);
-  updateData();
-  renderUIInfo();
-}
-
-
-function addAdjectif() {
-  let newAdjectif = document.getElementById("new_adjectif").value;
-  data.adjectifs = addWordIn(newAdjectif, data.adjectifs);
-  updateData();
-  renderUIInfo();
-}
-
-
-// Execute when DataReady is sent.
-function start() {
-  if (data === null) {
-    data = dataIfNoJSON;
-    console.log("Loaded static json from js");
-  }
-  renderUIInfo();
-  douxItte();
-}
-
-
-function init() {
-  setBtnInteractivity();
-  loadData();
-}
-
-window.addEventListener('DataReady', start);
-
-
-//  First load
-//$btn.dispatchEvent(new Event("click"));
-init();
+$('body').ripples({
+	resolution: 512,
+	dropRadius: 20,
+	perturbance: 0.04,
+});
